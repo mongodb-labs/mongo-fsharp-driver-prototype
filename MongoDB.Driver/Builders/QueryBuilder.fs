ccede55ff2ca7fa889ea20e945204391f720f864
Max Hirschhorn <max.hirschhorn@10gen.com>
2013-06-25 10:30:56 -0700

    Created new query builder.
    Refer to http://docs.mongodb.org/manual/reference/operator.
    Supports the following comparison operators:
      - all
      - eq
      - gt
      - gte
      - in
      - lt
      - lte
      - ne
      - nin
    
    Note that the static members are captialized to indicate semantically
    that they accept a sequence, which helps to avoid naming issues with
    the logical operators down the road.

---
+++ MongoDB.Driver/Builders/QueryBuilder.fs
@@ -0,0 +1,88 @@
+﻿namespace FSharp.MongoDB.Driver.Builders
+
+open System
+
+open MongoDB.Bson
+open MongoDB.Driver
+
+type Query =
+    static member init =
+        fun (name : string) ->
+            if name = null then
+                raise <| ArgumentNullException("name")
+            QueryDocument(name, BsonDocument())
+
+[<AutoOpen>]
+module Comparison =
+    type Query with
+        static member All (values : #seq<#obj>) (cont : string -> QueryDocument) =
+            fun (name : string) ->
+                let doc = name |> cont
+                let subdoc = doc.GetElement(name).Value :?> BsonDocument
+
+                subdoc.Add("$all", BsonArray(values)) |> ignore
+                doc
+
+        // TODO: handle case where the value is a sequence.
+        // REVIEW: why does ``BsonValue.Create/1`` not have a case for IEnumerable<_>?
+        static member eq value (name : string) =
+            if name = null then
+                raise <| ArgumentNullException("name")
+            QueryDocument(name, BsonValue.Create(value))
+
+        static member gt value (cont : string -> QueryDocument) =
+            fun (name : string) ->
+                let doc = name |> cont
+                let subdoc = doc.GetElement(name).Value :?> BsonDocument
+
+                subdoc.Add("$gt", BsonValue.Create(value)) |> ignore
+                doc
+
+        static member gte value (cont : string -> QueryDocument) =
+            fun (name : string) ->
+                let doc = name |> cont
+                let subdoc = doc.GetElement(name).Value :?> BsonDocument
+
+                subdoc.Add("$gte", BsonValue.Create(value)) |> ignore
+                doc
+
+        static member In (values : #seq<#obj>) (cont : string -> QueryDocument) =
+            fun (name : string) ->
+                let doc = name |> cont
+                let subdoc = doc.GetElement(name).Value :?> BsonDocument
+
+                subdoc.Add("$in", BsonArray(values)) |> ignore
+                doc
+
+        static member lt value (cont : string -> QueryDocument) =
+            fun (name : string) ->
+                let doc = name |> cont
+                let subdoc = doc.GetElement(name).Value :?> BsonDocument
+
+                subdoc.Add("$lt", BsonValue.Create(value)) |> ignore
+                doc
+
+        static member lte value (cont : string -> QueryDocument) =
+            fun (name : string) ->
+                let doc = name |> cont
+                let subdoc = doc.GetElement(name).Value :?> BsonDocument
+
+                subdoc.Add("$lte", BsonValue.Create(value)) |> ignore
+                doc
+
+        static member ne value (cont : string -> QueryDocument) =
+            fun (name : string) ->
+                let doc = name |> cont
+                let subdoc = doc.GetElement(name).Value :?> BsonDocument
+
+                subdoc.Add("$ne", BsonValue.Create(value)) |> ignore
+                doc
+
+        static member Nin (values : #seq<#obj>) (cont : string -> QueryDocument) =
+            fun (name : string) ->
+                let doc = name |> cont
+                let subdoc = doc.GetElement(name).Value :?> BsonDocument
+
+                subdoc.Add("$nin", BsonArray(values)) |> ignore
+                doc
+
---
+++ MongoDB.DriverUnitTests/Builders/QueryBuilderTests.fs
@@ -0,0 +1,77 @@
+﻿namespace FSharp.MongoDB.DriverUnitTests.Builders
+
+open System.Collections
+
+open MongoDB.Bson
+open MongoDB.Driver
+open NUnit.Framework
+open Swensen.Unquote
+
+open FSharp.MongoDB.Driver.Builders
+
+[<TestFixture>]
+module Comparison =
+
+    [<Test>]
+    let ``test all``() =
+        let query = <@ "tags" |> (Query.init |> Query.All ["appliances"; "school"; "book"]) @>
+        let expected = <@ QueryDocument("tags", BsonDocument("$all", BsonArray(["appliances"; "school"; "book"]))) @>
+
+        test <@ %query = %expected @>
+
+    [<Test>]
+    let ``test equal to``() =
+        let query = <@ "qty" |> Query.eq 20 @>
+        let expected = <@ QueryDocument("qty", BsonInt32(20)) @>
+
+        test <@ %query = %expected @>
+
+    [<Test>]
+    let ``test greater than``() =
+        let query = <@ "qty" |> (Query.init |> Query.gt 20) @>
+        let expected = <@ QueryDocument("qty", BsonDocument("$gt", BsonInt32(20))) @>
+
+        test <@ %query = %expected @>
+
+    [<Test>]
+    let ``test greater than or equal to``() =
+        let query = <@ "qty" |> (Query.init |> Query.gte 20) @>
+        let expected = <@ QueryDocument("qty", BsonDocument("$gte", BsonInt32(20))) @>
+
+        test <@ %query = %expected @>
+
+    [<Test>]
+    let ``test in``() =
+        let query = <@ "qty" |> (Query.init |> Query.In [5; 15]) @>
+        let expected = <@ QueryDocument("qty", BsonDocument("$in", BsonArray([5; 15]))) @>
+
+        test <@ %query = %expected @>
+
+    [<Test>]
+    let ``test less than``() =
+        let query = <@ "qty" |> (Query.init |> Query.lt 20) @>
+        let expected = <@ QueryDocument("qty", BsonDocument("$lt", BsonInt32(20))) @>
+
+        test <@ %query = %expected @>
+
+    [<Test>]
+    let ``test less than or equal to``() =
+        let query = <@ "qty" |> (Query.init |> Query.lte 20) @>
+        let expected = <@ QueryDocument("qty", BsonDocument("$lte", BsonInt32(20))) @>
+
+        test <@ %query = %expected @>
+
+    [<Test>]
+    let ``test not equal to``() =
+        let query = <@ "qty" |> (Query.init |> Query.ne 20) @>
+        let expected = <@ QueryDocument("qty", BsonDocument("$ne", BsonInt32(20))) @>
+
+        test <@ %query = %expected @>
+
+    [<Test>]
+    let ``test not in``() =
+        let query = <@ "qty" |> (Query.init |> Query.Nin [5; 15]) @>
+        let expected = <@ QueryDocument("qty", BsonDocument("$nin", BsonArray([5; 15]))) @>
+
+        test <@ %query = %expected @>
+
