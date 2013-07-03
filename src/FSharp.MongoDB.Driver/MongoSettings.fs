f5774b5c159e51bb1a1dc323aaded26fc09a284e
Max Hirschhorn <max.hirschhorn@10gen.com>
2013-07-03 10:01:05 -0700

    Created MongoSettings module.
    Uses record types to provide the default settings for the stream,
    connection, channel provider, and clusterable server factories.

---
+++ src/FSharp.MongoDB.Driver/MongoSettings.fs
@@ -0,0 +1,57 @@
+namespace FSharp.MongoDB.Driver
+
+open System
+open System.Threading
+
+module MongoSettings =
+
+    type StreamSettings = {
+        ConnectTimeout : TimeSpan
+        ReadTimeout : TimeSpan
+        TcpReceiveBufferSize : int
+        TcpSendBufferSize : int
+        WriteTimeout : TimeSpan
+    }
+
+    type ChannelProviderSettings = {
+        ConnectionMaxIdleTime : TimeSpan
+        ConnectionMaxLifeTime : TimeSpan
+        MaxSize : int
+        MinSize : int
+        SizeMaintenanceFrequency : TimeSpan
+        WaitQueueSize : int
+    }
+
+    type ClusterableServerSettings = {
+        ConnectRetryFrequency : TimeSpan
+        HeartbeatFrequency : TimeSpan
+        MaxDocumentSizeDefault : int
+        MaxMessageSizeDefault : int
+    }
+
+    module Defaults =
+        open System.Threading
+
+        let streamSettings = {
+            ConnectTimeout = TimeSpan.FromSeconds <| float 30
+            ReadTimeout = TimeSpan.FromMilliseconds <| float Timeout.Infinite // OS default
+            TcpReceiveBufferSize = 64 * 1024 // 64KiB (note: larger than 2MiB fails on Mac using Mono)
+            TcpSendBufferSize = 64 * 1024 // 64KiB (TODO: what is the optimum value for the buffers?)
+            WriteTimeout = TimeSpan.FromMilliseconds <| float Timeout.Infinite // OS default
+        }
+
+        let channelProviderSettings = {
+            ConnectionMaxIdleTime = TimeSpan.FromMinutes <| float 10
+            ConnectionMaxLifeTime = TimeSpan.FromMinutes <| float 30
+            MaxSize = 100
+            MinSize = 0
+            SizeMaintenanceFrequency = TimeSpan.FromMinutes <| float 1
+            WaitQueueSize = 500 // MaxSize * 5
+        }
+
+        let clusterableServerSettings = {
+            ConnectRetryFrequency = TimeSpan.FromSeconds <| float 2
+            HeartbeatFrequency = TimeSpan.FromSeconds <| float 10
+            MaxDocumentSizeDefault = 4 * 1024 * 1024
+            MaxMessageSizeDefault = 16000000 // 16MB (not 16 MiB!)
+        }
