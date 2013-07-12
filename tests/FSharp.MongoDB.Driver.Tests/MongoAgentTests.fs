9e589ff593790d4a5f7b62a8da793007975d12f9
Max Hirschhorn <max.hirschhorn@10gen.com>
2013-07-12 09:59:29 -0700

    Wrote tests for remove single, multi, and all.

--- tests/FSharp.MongoDB.Driver.Tests/MongoAgentTests.fs
+++ tests/FSharp.MongoDB.Driver.Tests/MongoAgentTests.fs
@@ -232,3 +232,128 @@ module UpdateOps =
                 test <@ elem.Value.AsInt32 - 1 = found.[elem.Name].AsInt32 @>
             else
                 test <@ elem.Value = found.[elem.Name] @>
+
+[<TestFixture>]
+module RemoveOps =
+
+    let clctn = "removeops"
+
+    [<TearDown>]
+    let ``drop collection``() =
+        agent.DropCollection db clctn |> Async.RunSynchronously |> ignore
+
+    [<Test>]
+    let ``test remove single``() =
+        let docs = [ BsonDocument([ BsonElement("_id", BsonInt32(11));
+                                    BsonElement("item", BsonString("pencil"));
+                                    BsonElement("qty", BsonInt32(50));
+                                    BsonElement("type", BsonString("no.2")) ]);
+                     BsonDocument([ BsonElement("item", BsonString("pen"));
+                                    BsonElement("qty", BsonInt32(20)) ]);
+                     BsonDocument([ BsonElement("item", BsonString("eraser"));
+                                    BsonElement("qty", BsonInt32(25)) ]) ]
+
+        let insertFlags = InsertFlags.None
+        let insertSettings = { MongoOperationSettings.Defaults.insertSettings with AssignIdOnInsert = true }
+
+        agent.BulkInsert db clctn docs insertFlags insertSettings |> Async.RunSynchronously |> ignore
+
+        let removeQuery = BsonDocument("_id", BsonInt32(11))
+
+        let deleteFlags = DeleteFlags.Single
+        let removeSettings = MongoOperationSettings.Defaults.removeSettings
+
+        agent.Remove db clctn removeQuery deleteFlags removeSettings |> Async.RunSynchronously |> ignore
+
+        let findQuery = BsonDocument()
+        let project = BsonDocument()
+
+        let queryFlags = QueryFlags.None
+        let querySettings = MongoOperationSettings.Defaults.querySettings
+
+        let res = agent.Find db clctn findQuery project 0 0 queryFlags querySettings |> Async.RunSynchronously |> Seq.toList
+
+        res.Length =? docs.Length - 1 // should have removed a single document from the collection
+        for (doc, found) in List.zip (docs |> List.tail) res do
+            // Check that found document contains correct values for elements of non-removed document
+            for elem in doc do
+                test <@ elem.Value = found.[elem.Name] @>
+
+            // Check that found document contains no other fields than previously examined
+            test <@ doc.ElementCount = found.ElementCount @>
+
+    [<Test>]
+    let ``test remove multi``() =
+        let docs = [ BsonDocument([ BsonElement("_id", BsonInt32(11));
+                                    BsonElement("item", BsonString("pencil"));
+                                    BsonElement("qty", BsonInt32(50));
+                                    BsonElement("type", BsonString("no.2")) ]);
+                     BsonDocument([ BsonElement("item", BsonString("pen"));
+                                    BsonElement("qty", BsonInt32(20)) ]);
+                     BsonDocument([ BsonElement("item", BsonString("eraser"));
+                                    BsonElement("qty", BsonInt32(25)) ]) ]
+
+        let insertFlags = InsertFlags.None
+        let insertSettings = { MongoOperationSettings.Defaults.insertSettings with AssignIdOnInsert = true }
+
+        agent.BulkInsert db clctn docs insertFlags insertSettings |> Async.RunSynchronously |> ignore
+
+        let removeQuery = BsonDocument("qty", BsonDocument("$lte", BsonInt32(25)))
+
+        let deleteFlags = DeleteFlags.None
+        let removeSettings = MongoOperationSettings.Defaults.removeSettings
+
+        agent.Remove db clctn removeQuery deleteFlags removeSettings |> Async.RunSynchronously |> ignore
+
+        let findQuery = BsonDocument()
+        let project = BsonDocument()
+
+        let queryFlags = QueryFlags.None
+        let querySettings = MongoOperationSettings.Defaults.querySettings
+
+        let res = agent.Find db clctn findQuery project 0 0 queryFlags querySettings |> Async.RunSynchronously |> Seq.toList
+
+        res.Length =? 1 // should have removed multiple documents from the collection
+        let found = res |> List.head
+
+        let doc = docs |> List.head
+
+        // Check that found document contains correct values for elements of non-removed document
+        for elem in doc do
+            test <@ elem.Value = found.[elem.Name] @>
+
+        // Check that found document contains no other fields than previously examined
+        test <@ doc.ElementCount = found.ElementCount @>
+
+    [<Test>]
+    let ``test remove all``() =
+        let docs = [ BsonDocument([ BsonElement("_id", BsonInt32(11));
+                                    BsonElement("item", BsonString("pencil"));
+                                    BsonElement("qty", BsonInt32(50));
+                                    BsonElement("type", BsonString("no.2")) ]);
+                     BsonDocument([ BsonElement("item", BsonString("pen"));
+                                    BsonElement("qty", BsonInt32(20)) ]);
+                     BsonDocument([ BsonElement("item", BsonString("eraser"));
+                                    BsonElement("qty", BsonInt32(25)) ]) ]
+
+        let insertFlags = InsertFlags.None
+        let insertSettings = { MongoOperationSettings.Defaults.insertSettings with AssignIdOnInsert = true }
+
+        agent.BulkInsert db clctn docs insertFlags insertSettings |> Async.RunSynchronously |> ignore
+
+        let removeQuery = BsonDocument()
+
+        let deleteFlags = DeleteFlags.None
+        let removeSettings = MongoOperationSettings.Defaults.removeSettings
+
+        agent.Remove db clctn removeQuery deleteFlags removeSettings |> Async.RunSynchronously |> ignore
+
+        let findQuery = BsonDocument()
+        let project = BsonDocument()
+
+        let queryFlags = QueryFlags.None
+        let querySettings = MongoOperationSettings.Defaults.querySettings
+
+        let res = agent.Find db clctn findQuery project 0 0 queryFlags querySettings |> Async.RunSynchronously |> Seq.toList
+
+        res.Length =? 0 // should have removed all documents from the collection
