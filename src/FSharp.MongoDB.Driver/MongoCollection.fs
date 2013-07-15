8218d18e63dce9f6a294197d31a665cb473a275d
Max Hirschhorn <max.hirschhorn@10gen.com>
2013-07-15 16:00:40 -0700

    Mocked up Fluent API for MongoCollection.
    Refer to:
      https://wiki.10gen.com/display/10gen/Fluent+Interface

--- src/FSharp.MongoDB.Driver/MongoCollection.fs
+++ src/FSharp.MongoDB.Driver/MongoCollection.fs
@@ -13,3 +13,48 @@ type MongoCollection(agent : MongoAgent, db, clctn) =
     member __.Update query update = agent.Update db clctn query update
 
     member __.Remove query = agent.Remove db clctn query
+
+module Fluent =
+
+    open MongoDB.Bson
+
+    type Scope = {
+        Query : BsonDocument option
+        Project : BsonDocument option
+        Sort : BsonDocument option
+        Limit : int
+        Skip : int
+    }
+
+    let defaultScope = {
+        Query = None
+        Project = None
+        Sort = None
+        Limit = 0
+        Skip = 0
+    }
+
+    [<RequireQualifiedAccess>]
+    module Query =
+
+        let find query scope =
+            { scope with Query = Some query }
+
+        let fields project scope =
+            { scope with Project = Some project }
+
+        let sort order scope =
+            { scope with Sort = Some order }
+
+        let limit n scope =
+            { scope with Limit = n }
+
+        let skip n scope =
+            { scope with Skip = n }
+
+    type MongoCollection with
+
+        member x.Find (?query0 : BsonDocument) =
+            let query = defaultArg query0 <| BsonDocument()
+
+            { defaultScope with Query = Some query }
