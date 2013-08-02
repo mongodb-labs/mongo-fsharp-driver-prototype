27b8d65f9627c9552e1e15a5dee22c1024046082
Max Hirschhorn <max.hirschhorn@10gen.com>
2013-08-02 10:24:14 -0700

    Wrote typed tests for all update operations.
    Have separate cases for usage of mutable and immutable types, as well
    as for infix operators that could be used in combination with a
    forward-pipe.
    
    Currently all of these tests fail, as support for typed update
    operations has not yet been implemented.

--- tests/FSharp.MongoDB.Driver.Tests/TypedQuotableMongoTests.fs
+++ tests/FSharp.MongoDB.Driver.Tests/TypedQuotableMongoTests.fs
@@ -264,3 +264,332 @@ module TypedQuotableMongo =
             let expected = <@ BsonDocument("other.duration", BsonInt32(45)) @>
 
             test <@ %query = %expected @>
+
+    module Update =
+
+        [<TestFixture>]
+        module Fields =
+
+            [<Test>]
+            let ``test typed mutable increment``() =
+                let update = <@ <@ fun (x : Mutable.Item) -> [ x.qty <- x.qty |> (+) 1 ] @> |> bson @>
+                let expected = <@ BsonDocument("$inc", BsonDocument("qty", BsonInt32(1))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed mutable increment infix``() =
+                let update = <@ <@ fun (x : Mutable.Item) -> [ x.qty <- x.qty + 1 ] @> |> bson @>
+                let expected = <@ BsonDocument("$inc", BsonDocument("qty", BsonInt32(1))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed immutable increment``() =
+                let update = <@ <@ fun (x : Immutable.Item) -> { x with qty = x.qty |> (+) 1 } @> |> bson @>
+                let expected = <@ BsonDocument("$inc", BsonDocument("qty", BsonInt32(1))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed immutable increment infix``() =
+                let update = <@ <@ fun (x : Immutable.Item) -> { x with qty = x.qty + 1 } @> |> bson @>
+                let expected = <@ BsonDocument("$inc", BsonDocument("qty", BsonInt32(1))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed mutable decrement``() =
+                let update = <@ <@ fun (x : Mutable.Item) -> [ x.qty <- x.qty |> (-) 2 ] @> |> bson @>
+                let expected = <@ BsonDocument("$inc", BsonDocument("qty", BsonInt32(-2))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed mutable decrement infix``() =
+                let update = <@ <@ fun (x : Mutable.Item) -> [ x.qty <- x.qty - 2 ] @> |> bson @>
+                let expected = <@ BsonDocument("$inc", BsonDocument("qty", BsonInt32(-2))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed immutable decrement``() =
+                let update = <@ <@ fun (x : Immutable.Item) -> { x with qty = x.qty |> (-) 2 } @> |> bson @>
+                let expected = <@ BsonDocument("$inc", BsonDocument("qty", BsonInt32(-2))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed immutable decrement infix``() =
+                let update = <@ <@ fun (x : Immutable.Item) -> { x with qty = x.qty - 2 } @> |> bson @>
+                let expected = <@ BsonDocument("$inc", BsonDocument("qty", BsonInt32(-2))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed mutable set``() =
+                let update = <@ <@ fun (x : Mutable.Item) -> [ x.qty <- 20 ] @> |> bson @>
+                let expected = <@ BsonDocument("$set", BsonDocument("qty", BsonInt32(20))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed immutable set``() =
+                let update = <@ <@ fun (x : Immutable.Item) -> { x with qty = 20 } @> |> bson @>
+                let expected = <@ BsonDocument("$set", BsonDocument("qty", BsonInt32(20))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed mutable set list``() =
+                let update = <@ <@ fun (x : Mutable.Item) -> [ x.tags <- [ "appliances"; "school"; "book" ] ] @> |> bson @>
+                let expected = <@ BsonDocument("$set", BsonDocument("tags", BsonArray([ "appliances"; "school"; "book" ]))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed immutable set list``() =
+                let update = <@ <@ fun (x : Immutable.Item) -> { x with tags = [ "appliances"; "school"; "book" ] } @> |> bson @>
+                let expected = <@ BsonDocument("$set", BsonDocument("tags", BsonArray([ "appliances"; "school"; "book" ]))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed mutable unset``() =
+                let update = <@ <@ fun (x : Mutable.Item) -> [ x.option <- None ] @> |> bson @>
+                let expected = <@ BsonDocument("$unset", BsonDocument("option", BsonInt32(1))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed immutable unset``() =
+                let update = <@ <@ fun (x : Immutable.Item) -> { x with option = None } @> |> bson @>
+                let expected = <@ BsonDocument("$unset", BsonDocument("option", BsonInt32(1))) @>
+
+                test <@ %update = %expected @>
+
+        [<TestFixture>]
+        module Array =
+
+            [<Test>]
+            let ``test typed mutable add to set``() =
+                let update = <@ <@ fun (x : Mutable.Item) -> [ x.tags <- x.tags |> Update.addToSet "toaster" ] @> |> bson @>
+                let expected = <@ BsonDocument("$addToSet", BsonDocument("tags", BsonString("toaster"))) @>
+
+                test <@  %update = %expected @>
+
+            [<Test>]
+            let ``test typed immutable add to set``() =
+                let update = <@ <@ fun (x : Immutable.Item) -> { x with tags = x.tags |> Update.addToSet "toaster" } @> |> bson @>
+                let expected = <@ BsonDocument("$addToSet", BsonDocument("tags", BsonString("toaster"))) @>
+
+                test <@  %update = %expected @>
+
+            [<Test>]
+            let ``test typed mutable pop left``() =
+                let update = <@ <@ fun (x : Mutable.Item) -> [ x.sizes <- x.sizes |> Update.popleft ] @> |> bson @>
+                let expected = <@ BsonDocument("$pop", BsonDocument("sizes", BsonInt32(-1))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed immutable pop left``() =
+                let update = <@ <@ fun (x : Immutable.Item) -> { x with sizes = x.sizes |> Update.popleft } @> |> bson @>
+                let expected = <@ BsonDocument("$pop", BsonDocument("sizes", BsonInt32(-1))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed mutable pop right``() =
+                let update = <@ <@ fun (x : Mutable.Item) -> [ x.sizes <- x.sizes |> Update.popright ] @> |> bson @>
+                let expected = <@ BsonDocument("$pop", BsonDocument("sizes", BsonInt32(1))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed immutable pop right``() =
+                let update = <@ <@ fun (x : Immutable.Item) -> { x with sizes = x.sizes |> Update.popright } @> |> bson @>
+                let expected = <@ BsonDocument("$pop", BsonDocument("sizes", BsonInt32(1))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed mutable pull``() =
+                let update = <@ <@ fun (x : Mutable.Item) -> [ x.sizes <- x.sizes |> Update.pull (bson <@ fun (y : Mutable.Size) -> y.height = 75 @>) ] @> |> bson @>
+                let expected = <@ BsonDocument("$pull", BsonDocument("sizes", BsonDocument("height", BsonInt32(75)))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed immutable pull``() =
+                let update = <@ <@ fun (x : Immutable.Item) -> { x with sizes = x.sizes |> Update.pull (bson <@ fun (y : Immutable.Size) -> y.height = 75 @>) } @> |> bson @>
+                let expected = <@ BsonDocument("$pull", BsonDocument("sizes", BsonDocument("height", BsonInt32(75)))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed mutable pull all``() =
+                let update = <@ <@ fun (x : Mutable.Item) -> [ x.tags <- x.tags |> Update.pullAll [ "appliances"; "school"; "book" ] ] @> |> bson @>
+                let expected = <@ BsonDocument("$pullAll", BsonDocument("tags", BsonArray([ "appliances"; "school"; "book" ]))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed immutable pull all``() =
+                let update = <@ <@ fun (x : Immutable.Item) -> { x with tags = x.tags |> Update.pullAll [ "appliances"; "school"; "book" ] } @> |> bson @>
+                let expected = <@ BsonDocument("$pullAll", BsonDocument("tags", BsonArray([ "appliances"; "school"; "book" ]))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed mutable push``() =
+                let update = <@ <@ fun (x : Mutable.Item) -> [ x.tags <- x.tags |> Update.push "toaster" ] @> |> bson @>
+                let expected = <@ BsonDocument("$push", BsonDocument("tags", BsonString("toaster"))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed immutable push``() =
+                let update = <@ <@ fun (x : Immutable.Item) -> { x with tags = x.tags |> Update.push "toaster" } @> |> bson @>
+                let expected = <@ BsonDocument("$push", BsonDocument("tags", BsonString("toaster"))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed mutable add to set with each modifier``() =
+                let update = <@ <@ fun (x : Mutable.Item) -> [ x.tags <- x.tags |> Update.each Update.addToSet [ "appliances"; "school"; "book" ] ] @> |> bson @>
+                let expected = <@ BsonDocument("$addToSet", BsonDocument("tags", BsonDocument("$each", BsonArray([ "appliances"; "school"; "book" ])))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed immutable add to set with each modifier``() =
+                let update = <@ <@ fun (x : Immutable.Item) -> { x with tags = x.tags |> Update.each Update.addToSet [ "appliances"; "school"; "book" ] } @> |> bson @>
+                let expected = <@ BsonDocument("$addToSet", BsonDocument("tags", BsonDocument("$each", BsonArray([ "appliances"; "school"; "book" ])))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed mutable push with each modifier``() =
+                let update = <@ <@ fun (x : Mutable.Item) -> [ x.tags <- x.tags |> Update.each Update.push [ "appliances"; "school"; "book" ] ] @> |> bson @>
+                let expected = <@ BsonDocument("$push", BsonDocument("tags", BsonDocument("$each", BsonArray([ "appliances"; "school"; "book" ])))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed immutable push with each modifier``() =
+                let update = <@ <@ fun (x : Immutable.Item) -> { x with tags = x.tags |> Update.each Update.push [ "appliances"; "school"; "book" ] } @> |> bson @>
+                let expected = <@ BsonDocument("$push", BsonDocument("tags", BsonDocument("$each", BsonArray([ "appliances"; "school"; "book" ])))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed mutable push with slice modifier``() =
+                let update = <@ <@ fun (x : Mutable.Item) -> [ x.tags <- x.tags |> Update.each Update.push [ "appliances"; "school"; "book" ]
+                                                                                |> Update.slice -5 ] @> |> bson @>
+
+                let expected = <@ BsonDocument("$push", BsonDocument("tags", BsonDocument([ BsonElement("$each", BsonArray([ "appliances"; "school"; "book" ]))
+                                                                                            BsonElement("$slice", BsonInt32(-5)) ]))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed immutable push with slice modifier``() =
+                let update = <@ <@ fun (x : Immutable.Item) -> { x with tags = x.tags |> Update.each Update.push [ "appliances"; "school"; "book" ]
+                                                                                      |> Update.slice -5 } @> |> bson @>
+
+                let expected = <@ BsonDocument("$push", BsonDocument("tags", BsonDocument([ BsonElement("$each", BsonArray([ "appliances"; "school"; "book" ]))
+                                                                                            BsonElement("$slice", BsonInt32(-5)) ]))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed mutable push with sort modifier``() =
+                let size1 = { Mutable.Size.length = 3; Mutable.Size.width = 8; Mutable.Size.height = 1 }
+                let size2 = { Mutable.Size.length = 4; Mutable.Size.width = 7; Mutable.Size.height = 2 }
+                let size3 = { Mutable.Size.length = 5; Mutable.Size.width = 6; Mutable.Size.height = 4 }
+
+                let update = <@ <@ fun (x : Mutable.Item) -> [ x.sizes <- x.sizes |> Update.each Update.push [ size1; size2; size3 ]
+                                                                                  |> Update.sort (bson <@ fun (y : BsonDocument) -> y?width = 1 @>)
+                                                                                  |> Update.slice -5 ] @> |> bson @>
+
+                let expected = <@ BsonDocument("$push", BsonDocument("sizes", BsonDocument([ BsonElement("$each", BsonArray([ size1; size2; size3 ]))
+                                                                                             BsonElement("$sort", BsonDocument("width", BsonInt32(1)))
+                                                                                             BsonElement("$slice", BsonInt32(-5)) ]))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed immutable push with sort modifier``() =
+                let size1 = { Immutable.Size.length = 3; Immutable.Size.width = 8; Immutable.Size.height = 1 }
+                let size2 = { Immutable.Size.length = 4; Immutable.Size.width = 7; Immutable.Size.height = 2 }
+                let size3 = { Immutable.Size.length = 5; Immutable.Size.width = 6; Immutable.Size.height = 4 }
+
+                let update = <@ <@ fun (x : Immutable.Item) -> { x with sizes = x.sizes |> Update.each Update.push [ size1; size2; size3 ]
+                                                                                        |> Update.sort (bson <@ fun (y : BsonDocument) -> y?width = 1 @>)
+                                                                                        |> Update.slice -5 } @> |> bson @>
+
+                let expected = <@ BsonDocument("$push", BsonDocument("sizes", BsonDocument([ BsonElement("$each", BsonArray([ size1; size2; size3 ]))
+                                                                                             BsonElement("$sort", BsonDocument("width", BsonInt32(1)))
+                                                                                             BsonElement("$slice", BsonInt32(-5)) ]))) @>
+
+                test <@ %update = %expected @>
+
+        [<TestFixture>]
+        module Bitwise =
+
+            [<Test>]
+            let ``test typed mutable bitwise and``() =
+                let update = <@ <@ fun (x : Mutable.Item) -> [ x.qty <- x.qty |> (&&&) 5 ] @> |> bson @>
+                let expected = <@ BsonDocument("$bit", BsonDocument("qty", BsonDocument("and", BsonInt32(5)))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed mutable bitwise and infix``() =
+                let update = <@ <@ fun (x : Mutable.Item) -> [ x.qty <- x.qty &&& 5 ] @> |> bson @>
+                let expected = <@ BsonDocument("$bit", BsonDocument("qty", BsonDocument("and", BsonInt32(5)))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed immutable bitwise and``() =
+                let update = <@ <@ fun (x : Immutable.Item) -> { x with qty = x.qty |> (&&&) 5 } @> |> bson @>
+                let expected = <@ BsonDocument("$bit", BsonDocument("qty", BsonDocument("and", BsonInt32(5)))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed immutable bitwise and infix``() =
+                let update = <@ <@ fun (x : Immutable.Item) -> { x with qty = x.qty &&& 5 } @> |> bson @>
+                let expected = <@ BsonDocument("$bit", BsonDocument("qty", BsonDocument("and", BsonInt32(5)))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed mutable bitwise or``() =
+                let update = <@ <@ fun (x : Mutable.Item) -> [ x.qty <- x.qty |> (|||) 5 ] @> |> bson @>
+                let expected = <@ BsonDocument("$bit", BsonDocument("qty", BsonDocument("or", BsonInt32(5)))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed mutable bitwise or infix``() =
+                let update = <@ <@ fun (x : Mutable.Item) -> [ x.qty <- x.qty ||| 5 ] @> |> bson @>
+                let expected = <@ BsonDocument("$bit", BsonDocument("qty", BsonDocument("or", BsonInt32(5)))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed immutable bitwise or``() =
+                let update = <@ <@ fun (x : Immutable.Item) -> { x with qty = x.qty |> (|||) 5 } @> |> bson @>
+                let expected = <@ BsonDocument("$bit", BsonDocument("qty", BsonDocument("or", BsonInt32(5)))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed immutable bitwise or infix``() =
+                let update = <@ <@ fun (x : Immutable.Item) -> { x with qty = x.qty ||| 5 } @> |> bson @>
+                let expected = <@ BsonDocument("$bit", BsonDocument("qty", BsonDocument("or", BsonInt32(5)))) @>
+
+                test <@ %update = %expected @>
