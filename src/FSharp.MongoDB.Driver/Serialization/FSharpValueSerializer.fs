3a730c512485b5b65f6868cadf395b42680f882e
Max Hirschhorn <max.hirschhorn@10gen.com>
2013-08-05 17:32:19 -0700

    Wrote tests for record type de/serialization.
    Only simple verification of primitive-based record type. Also
    restructured the Serialization namespace so that the RecordSerializer
    could be unit tested.

--- src/FSharp.MongoDB.Driver/Serialization/FSharpValueSerializer.fs
+++ src/FSharp.MongoDB.Driver/Serialization/FSharpValueSerializer.fs
@@ -8,41 +8,41 @@ open MongoDB.Bson.Serialization.Serializers
 
 module Serialization =
 
+    [<AutoOpen>]
+    module internal Bson =
+
+        type RecordTypeSerializer(typ : System.Type) =
+            inherit BsonBaseSerializer()
+
+            let fields =
+                FSharpType.GetRecordFields(typ)
+                |> Seq.map (fun (x : System.Reflection.PropertyInfo) -> x.Name)
+
+            override __.Serialize(writer, nominalType, value, options) =
+                let values = FSharpValue.GetRecordFields(value)
+
+                writer.WriteStartDocument()
+
+                Seq.iter2 (fun field value ->
+                    writer.WriteName(field)
+                    BsonSerializer.Serialize(writer, value)
+                ) fields values
+
+                writer.WriteEndDocument()
+
+            override __.Deserialize(reader, nominalType, actualType, options) =
+                invalidOp "not implemented"
+
+        type FSharpTypeSerializationProvider() =
+
+            interface IBsonSerializationProvider with
+                member __.GetSerializer(typ : System.Type) =
+                    if FSharpType.IsRecord(typ) then
+                        RecordTypeSerializer(typ) :> IBsonSerializer
+                    else null
+
     [<RequireQualifiedAccess>]
     module FSharpValueSerializer =
-
-        [<AutoOpen>]
-        module private Bson =
-
-            type RecordTypeSerializer(typ : System.Type) =
-                inherit BsonBaseSerializer()
-
-                let fields =
-                    FSharpType.GetRecordFields(typ)
-                    |> Seq.map (fun (x : System.Reflection.PropertyInfo) -> x.Name)
-
-                override __.Serialize(writer, nominalType, value, options) =
-                    let values = FSharpValue.GetRecordFields(value)
-
-                    writer.WriteStartDocument()
-
-                    Seq.iter2 (fun field value ->
-                        writer.WriteName(field)
-                        BsonSerializer.Serialize(writer, value)
-                    ) fields values
-
-                    writer.WriteEndDocument()
-
-                override __.Deserialize(reader, nominalType, actualType, options) =
-                    invalidOp "not implemented"
-
-            type FSharpTypeSerializationProvider() =
-
-                interface IBsonSerializationProvider with
-                    member __.GetSerializer(typ : System.Type) =
-                        if FSharpType.IsRecord(typ) then
-                            RecordTypeSerializer(typ) :> IBsonSerializer
-                        else null
 
         let mutable private registered = false
 
