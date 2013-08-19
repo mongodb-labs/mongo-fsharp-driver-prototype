406bcef8e3da18bcf151b505038c3c0b5ce8a061
Max Hirschhorn <max.hirschhorn@10gen.com>
2013-08-19 10:29:23 -0700

    Extended support for typed collections and update operations.
    Note that in F# 3.0, the type inference system requires an annotation
    on the document when used by other members in the computation
    expression, regardless of that fact it maintains the variable space.
    This appears to have been resolved in F# 3.1 (see the Visual Studio F#
    Team Blog).
    
    The return type of the MongoQueryBuilder will be changed to
    MongoOperationResult, which will either return a Scope type or actually
    execute the operation specified (depending on query/update/etc.).

--- src/FSharp.MongoDB.Driver/ExpressibleMongo.fs
+++ src/FSharp.MongoDB.Driver/ExpressibleMongo.fs
@@ -7,6 +7,8 @@ open Microsoft.FSharp.Reflection
 
 open MongoDB.Bson
 
+open MongoDB.Driver.Core
+
 open FSharp.MongoDB.Driver.Quotations
 
 module Expression =
@@ -14,37 +16,144 @@ module Expression =
     type IMongoQueryable<'a> =
         inherit seq<'a>
 
+    type IMongoUpdatable<'a> =
+        inherit seq<'a>
+
+    [<RequireQualifiedAccess>]
+    type private TransformResult = // (fun * args * cont)
+       | Query of (Var -> Expr -> BsonElement) * (Var * Expr) * Expr
+       | Update of (Var -> string -> Expr -> BsonElement) * (Var * string * Expr) * Expr
+
+    [<RequireQualifiedAccess>]
+    type private TraverseResult = // (fun * args)
+       | Query of (Var -> Expr -> BsonElement) * (Var * Expr)
+       | Update of (Var -> string -> Expr -> BsonElement) * (Var * string * Expr)
+
+    [<RequireQualifiedAccess>]
+    type MongoOperationResult<'a> =
+       | Query of Scope<'a>
+       | Update of WriteConcernResult
+
     type MongoQueryBuilder() =
 
-        member __.Source (x : seq<'a>) = x :?> IMongoQueryable<'a>
+        let mkCall op args =
+            match <@ ignore %op @> with
+            | SpecificCall <@ ignore @> (_, _, [ Lambdas (_, Call (target, info, _)) ]) ->
+                match target with
+                | Some x -> Expr.Call(x, info, args)
+                | None -> Expr.Call(info, args)
+            | _ -> failwith "unable to acquire methodinfo"
 
-        member __.For (source : IMongoQueryable<'a>, f) = source |> Seq.collect f :?> IMongoQueryable<'a>
+        let rec (|List|_|) expr =
+            match expr with
+            | NewUnionCase (uci, args) when isListUnionCase uci ->
+                match args with
+                | [] -> Some([], typeof<unit>)
+                | [ head; List (tail, _) ] -> Some(head :: tail, typeof<Expr>)
+                | _ -> failwith "unexpected list union case"
 
-        member __.Yield v = [v]
+            | _ -> None
+
+        let (|ValueOrList|_|) expr =
+            match expr with
+            | Value (value, typ) -> Some (value, typ)
+            | List (values, typ) -> Some (box values, typ)
+            | _ -> None
+
+        let transform (x : MongoQueryBuilder) expr =
+            match expr with
+            // Query operations
+            | SpecificCall <@ x.Where @> (_, _, [ cont; Lambda (var, body) ]) ->
+                let res = TransformResult.Query (queryParser, (var, body), cont)
+                Some res
+
+            // Update operations
+            | SpecificCall <@ x.Set @> (_, _, [ cont; Lambda (_, body); ValueOrList (value, typ) ]) ->
+                match body with
+                | GetField (var, field) ->
+                    let res = TransformResult.Update (updateParser, (var, field, Expr.Value value), cont)
+                    Some res
+                | _ -> failwithf "unrecognized expression\n%A" body
+
+            | SpecificCall <@ x.Inc @> (_, _, [ cont; Lambda (_, body); Int32 (value) ]) ->
+                let (var, field, body) =
+                    match body with
+                    | CallDynamic (var, field) -> (var, field, Expr.Coerce(body, typeof<int>))
+                    | GetProperty (var, field) -> (var, field, body)
+                    | _ -> failwithf "unrecognized expression\n%A" body
+
+                let call = mkCall <@ (+) @> [ body; Expr.Value value ]
+                let res = TransformResult.Update (updateParser, (var, field, call), cont)
+                Some res
+
+            | SpecificCall <@ x.Dec @> (_, _, [ cont; Lambda (_, body); Int32 (value) ]) ->
+                let (var, field, body) =
+                    match body with
+                    | CallDynamic (var, field) -> (var, field, Expr.Coerce(body, typeof<int>))
+                    | GetProperty (var, field) -> (var, field, body)
+                    | _ -> failwithf "unrecognized expression\n%A" body
+
+                let call = mkCall <@ (-) @> [ body; Expr.Value value ]
+                let res = TransformResult.Update (updateParser, (var, field, call), cont)
+                Some res
+
+            | _ -> None
+
+        let traverse (x : MongoQueryBuilder) expr =
+            let rec traverse builder expr res =
+                match transform builder expr with
+                | Some trans ->
+                    match trans with
+                    | TransformResult.Query (f, args, cont) ->
+                        let x = TraverseResult.Query (f, args)
+                        traverse builder cont (x :: res)
+
+                    | TransformResult.Update (f, args, cont) ->
+                        let x = TraverseResult.Update (f, args)
+                        traverse builder cont (x :: res)
+
+                | None -> res
+
+            traverse x expr []
+
+        member __.Source (x : #seq<'a>) : IMongoQueryable<'a> = invalidOp "not implemented"
+
+        member __.For (source : IMongoQueryable<'a>, f : 'a -> IMongoQueryable<'a>) = invalidOp "not implemented"
+
+        member __.Zero () : IMongoQueryable<'a> = invalidOp "not implemented"
+
+        member __.Yield x : IMongoQueryable<'a> = invalidOp "not implemented"
 
         member __.Quote (expr : Expr<#seq<'a>>) = expr
 
         member x.Run (expr : Expr<#seq<'a>>) =
-            let rec (|NestedWhere|_|) expr =
-                match expr with
-                // REVIEW: should we $and the conditions together?
-                | SpecificCall <@ x.Where @> (_, _, [ NestedWhere _; q ]) ->
-                    let casted : Expr<'a -> bool> = q |> Expr.Cast
-                    Some casted
+            let elems =
+                traverse x expr
+                |> List.map (fun x ->
+                    match x with
+                    | TraverseResult.Query (f, x) -> x ||> f
+                    | TraverseResult.Update (f, x) -> x |||> f)
 
-                | SpecificCall <@ x.Where @> (_, _, [ _; q ]) ->
-                    let casted : Expr<'a -> bool> = q |> Expr.Cast
-                    Some casted
-
-                | _ -> None
-
-            match expr with
-            | NestedWhere (q) -> bson q
-
-            | _ -> failwith "unsupported operation"
+            BsonDocument(elems)
 
         [<CustomOperation("where", MaintainsVariableSpace = true)>]
-        member __.Where (source : IMongoQueryable<'a>, [<ProjectionParameter>] predicate) =
-            source |> Seq.filter predicate :?> IMongoQueryable<'a>
+        member __.Where (source : IMongoQueryable<'a>, [<ProjectionParameter>] predicate : 'a -> bool) : IMongoQueryable<'a> =
+            invalidOp "not implemented"
+
+        [<CustomOperation("update", MaintainsVariableSpace = true)>]
+        member __.Update (source : IMongoQueryable<'a>) : IMongoUpdatable<'a> =
+            invalidOp "not implemented"
+
+        [<CustomOperation("set", MaintainsVariableSpace = true)>]
+        member __.Set (source : IMongoUpdatable<'a>, [<ProjectionParameter>] field : 'a -> 'b, value : 'b) : IMongoUpdatable<'a> =
+            invalidOp "not implemented"
+
+        [<CustomOperation("inc", MaintainsVariableSpace = true)>]
+        member __.Inc (source : IMongoUpdatable<'a>, [<ProjectionParameter>] field : 'a -> 'b, value : 'b) : IMongoUpdatable<'a> =
+            invalidOp "not implemented"
+
+        [<CustomOperation("dec", MaintainsVariableSpace = true)>]
+        member __.Dec (source : IMongoUpdatable<'a>, [<ProjectionParameter>] field : 'a -> 'b, value : 'b) : IMongoUpdatable<'a> =
+            invalidOp "not implemented"
 
     let mongo = new MongoQueryBuilder()
--- tests/FSharp.MongoDB.Driver.Tests/ExpressibleMongoTests.fs
+++ tests/FSharp.MongoDB.Driver.Tests/ExpressibleMongoTests.fs
@@ -8,7 +8,33 @@ open Swensen.Unquote
 open FSharp.MongoDB.Driver.Expression
 open FSharp.MongoDB.Driver.Quotations
 
+open FSharp.MongoDB.Driver
+
 module ExpressibleMongo =
+
+    [<RequireQualifiedAccess>]
+    module Immutable =
+
+        type Item = {
+            tags : string list
+            qty : int
+            price : float
+            sale : bool
+            desc : Desc
+            sizes : Size list
+            option : unit option
+        }
+
+        and Desc = {
+            short : string
+            long : string
+        }
+
+        and Size = {
+            length : int
+            width : int
+            height : int
+        }
 
     module Query =
 
@@ -27,3 +53,105 @@ module ExpressibleMongo =
                 let expected = <@ BsonDocument("tags", BsonDocument("$all", BsonArray([ "appliances"; "school"; "book" ]))) @>
 
                 test <@ %query = %expected @>
+
+            [<Test>]
+            let ``test typed mongo query expression all``() =
+                let clctn : seq<Immutable.Item> = Seq.empty |> Seq.cast
+
+                let query = <@ mongo {
+                    for x in clctn do
+                    where ((x : Immutable.Item).tags |> Query.all [ "appliances"; "school"; "book" ])
+                } @>
+
+                let expected = <@ BsonDocument("tags", BsonDocument("$all", BsonArray([ "appliances"; "school"; "book" ]))) @>
+
+                test <@ %query = %expected @>
+
+    module Update =
+
+        [<TestFixture>]
+        module Fields =
+
+            [<Test>]
+            let ``test mongo query expression increment``() =
+                let clctn : seq<BsonDocument> = Seq.empty |> Seq.cast
+
+                let update = <@ mongo {
+                    for x in clctn do
+                    update
+                    inc x?qty 1
+                } @>
+
+                let expected = <@ BsonDocument("$inc", BsonDocument("qty", BsonInt32(1))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed mongo query expression increment``() =
+                let clctn : seq<Immutable.Item> = Seq.empty |> Seq.cast
+
+                let update = <@ mongo {
+                    for x in clctn do
+                    update
+                    inc (x : Immutable.Item).qty 1
+                } @>
+
+                let expected = <@ BsonDocument("$inc", BsonDocument("qty", BsonInt32(1))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test mongo query expression decrement``() =
+                let clctn : seq<BsonDocument> = Seq.empty |> Seq.cast
+
+                let update = <@ mongo {
+                    for x in clctn do
+                    update
+                    dec x?qty 2
+                } @>
+
+                let expected = <@ BsonDocument("$inc", BsonDocument("qty", BsonInt32(-2))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed mongo query expression decrement``() =
+                let clctn : seq<Immutable.Item> = Seq.empty |> Seq.cast
+
+                let update = <@ mongo {
+                    for x in clctn do
+                    update
+                    dec (x : Immutable.Item).qty 2
+                } @>
+
+                let expected = <@ BsonDocument("$inc", BsonDocument("qty", BsonInt32(-2))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test mongo query expression set``() =
+                let clctn : seq<BsonDocument> = Seq.empty |> Seq.cast
+
+                let update = <@ mongo {
+                    for x in clctn do
+                    update
+                    set x?qty 20
+                } @>
+
+                let expected = <@ BsonDocument("$set", BsonDocument("qty", BsonInt32(20))) @>
+
+                test <@ %update = %expected @>
+
+            [<Test>]
+            let ``test typed mongo query expression set``() =
+                let clctn : seq<Immutable.Item> = Seq.empty |> Seq.cast
+
+                let update = <@ mongo {
+                    for x in clctn do
+                    update
+                    set (x : Immutable.Item).qty 20
+                } @>
+
+                let expected = <@ BsonDocument("$set", BsonDocument("qty", BsonInt32(20))) @>
+
+                test <@ %update = %expected @>
